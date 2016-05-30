538b0964f3a8a136f51e2aa0ca1dd498225def4e
Georgiy on home pc <stepanov.g@hotmail.com>
2016-05-30 10:23:51 +0300

    Перечисление массивов и коллекций

--- WebApplication1-RazorLearning/Controllers/HomeController.cs
+++ WebApplication1-RazorLearning/Controllers/HomeController.cs
@@ -34,5 +34,17 @@ namespace WebApplication1_RazorLearning.Controllers
             ViewBag.Supplier = null;
             return View(myProduct);
         }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price= 275M },
+                new Product {Name = "Lifejacket", Price= 48.95M },
+                new Product {Name = "Kayak", Price= 275M },
+                new Product {Name = "Corner flag", Price= 34.95M }
+            };
+            return View(array);
+        }
     }
 }
